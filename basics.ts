be8f1f591100033f5b987a732526366bc2493f7a
Tyler Fenton <tsfento@gmail.com>
2023-09-24 20:14:22 -0500

    init commit

---
+++ basics.ts
@@ -0,0 +1,11 @@
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].split(''); errors with generic type set
